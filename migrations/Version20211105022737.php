6226d39b8a42e687a6ef685e9084a798bea4c34f
Ramiro Anacona Meneses <ranacona@hugeinc.com>
2021-11-05 16:48:00 -0500

    we will use a bundle to save simple config variables in the database like the pages that were already visited

---
+++ migrations/Version20211105022737.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20211105022737 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE craue_config_setting (name VARCHAR(255) NOT NULL, section VARCHAR(255) DEFAULT NULL, value VARCHAR(255) DEFAULT NULL, PRIMARY KEY(name))');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP TABLE craue_config_setting');
+    }
+}
---
+++ src/DataFixtures/ConfigSettingFixtures.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Craue\ConfigBundle\Entity\Setting;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class ConfigSettingFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $settings = [
+            'api_films_queried_pages' => null,
+            'api_characters_queried_pages' => null,
+        ];
+
+        foreach ($settings as $_setting => $value) {
+            $setting = new Setting();
+            $setting->setName($_setting);
+            $setting->setValue($value);
+
+            $manager->persist($setting);
+        }
+
+        $manager->flush();
+    }
+}
